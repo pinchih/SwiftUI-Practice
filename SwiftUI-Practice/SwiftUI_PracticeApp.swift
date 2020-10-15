2b4f2c9f75a6a07a802741b9c00e409b129f14a4
Pin-Chih Lin <tobeaceo@gmail.com>
2020-10-15 18:40:06 -0500

    Initial Commit

---
+++ SwiftUI-Practice/SwiftUI_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PracticeApp.swift
+//  SwiftUI-Practice
+//
+//  Created by Pin-Chih Lin on 10/15/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
